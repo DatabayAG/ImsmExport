1898e99a3f36c7214f83cc8c5ad318f56ee96584
fneumann <fneumann@databay.de>
2025-11-10 10:42:38 +0100

    Update for ILIAS 11

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id                = 'texpimsm';
 $version           = '5.0.0';
-$ilias_min_version = '10.3';
-$ilias_max_version = '10.999';
+$ilias_min_version = '11.0';
+$ilias_max_version = '11.999';
 $responsible       = 'Michael Jansen';
 $responsible_mail  = 'mjansen@databay.de';
