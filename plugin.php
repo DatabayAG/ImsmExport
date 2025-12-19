d4d76dc9a07c7fb5c449c4d5a2005a1128af5771
fneumann <fneumann@databay.de>
2025-12-19 09:31:14 +0100

    Version 2.1.0
    Integrated patch for essay questions

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                = 'texpimsm';
-$version           = '2.0.1';
+$version           = '2.1.0';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '7.999';
 $responsible       = 'Michael Jansen';
