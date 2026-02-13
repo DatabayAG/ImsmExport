6548e88d43883f3590fd304dd9bf1110e3c175b4
Rachid Rabah <rrbn@users.noreply.github.com>
2026-02-13 15:47:04 +0100

    Add require statement for ilImsmExportConfig

--- classes/class.ilImsmExportPlugin.php
+++ classes/class.ilImsmExportPlugin.php
@@ -2,7 +2,7 @@
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 require_once(__DIR__ . '/class.ilImsmExportHelper.php');
-
+require_once(__DIR__ . '/class.ilImsmExportConfig.php');
 /**
  * Abstract parent class for all event hook plugin classes.
  * @author Michael Jansen <mjansen@databay.de>
