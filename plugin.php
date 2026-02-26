025327055f8543aab4b91047ea5449704b5a12de
fneumann <fneumann@databay.de>
2026-02-26 09:17:17 +0100

    Version 5.0.1
    - Sort columns by test sequence

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                = 'texpimsm';
-$version           = '5.0.0';
+$version           = '5.0.1';
 $ilias_min_version = '10.0';
 $ilias_max_version = '10.999';
 $responsible       = 'Michael Jansen';
